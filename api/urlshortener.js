bbe091eed9ea63635c8d5d699284d9e0d640c09a
Nuno Gomes <nunomrgomes@gmail.com>
2021-08-15 22:21:17 +0100

    feat: url shortener

--- api/urlshortener.js
+++ api/urlshortener.js
@@ -1,19 +1,65 @@
+const shortid = require("shortid");
+const mongoose = require("mongoose");
+
+/* 
+  The approach:
+  const personSchema = new mongoose.Schema({...})
+  would also work
+*/
+const { Schema } = mongoose;
+const url = new Schema({
+  original_url: { type: String, required: true },
+  short_url: { type: String, required: true },
+});
+
+const URL = mongoose.model("URL", url);
+
 function getUrlShortenerHTML(req, res) {
-  res.sendFile(process.cwd() + '/views/urlshortener.html');
-};
+  res.sendFile(process.cwd() + "/views/urlshortener.html");
+}
 
-function shortenUrl(req, res) {
-	console.log("Post Request")
-	console.log(req.params)
+function setShortUrl(req, res) {
+  URL.findOne({ original_url: req.body.url }, (error, data) => {
+    if (error) return console.log(error);
+    if (!data) {
+      const short_url = shortid.generate();
+      const original_url = req.body.url;
+      const newUrl = new URL({
+        short_url,
+        original_url,
+      });
 
-    res.json({
-        success: true
-    });
-    // const { url }  = req.body;
-    // dns.lookup(url, (err, addresses) => res.json({ original_url: url, short_url: err}))
-};
+      newUrl.save((error, data) => {
+        if (error) return console.log(error);
+        res.json({
+          original_url,
+          short_url,
+        });
+      });
+    } else {
+      let { short_url, original_url } = data;
+      res.json({
+        original_url,
+        short_url,
+      });
+    }
+  });
+}
+
+function navigateToUrl(req, res) {
+  URL.findOne({ short_url: req.params.url }, (error, data) => {
+    if (error) return console.log(error);
+    console.log(data);
+    if (!data) {
+      res.json({ error: "No short URL found for the given input" });
+    } else {
+      res.redirect(data.original_url);
+    }
+  });
+}
 
 module.exports = {
-    getUrlShortenerHTML,
-    shortenUrl
-}
+  getUrlShortenerHTML,
+  setShortUrl,
+  navigateToUrl,
+};
