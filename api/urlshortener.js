831bac5c49e20edec94a203bd7ea0423a0442c44
TheNunoGomes <nunomrgomes@gmail.com>
2021-08-16 22:45:06 +0100

    reverting to mine

--- api/urlshortener.js
+++ api/urlshortener.js
@@ -12,43 +12,56 @@ const url = new Schema({
   short_url: { type: String, required: true },
 });
 
-const urlSchema = new mongoose.Schema({
-  url: String,
-});
-
-const Url = mongoose.model("Url", urlSchema);
+const URL = mongoose.model("URL", url);
 
 function getUrlShortenerHTML(req, res) {
   res.sendFile(process.cwd() + "/views/urlshortener.html");
 }
 
 function setShortUrl(req, res) {
-  const urlbody = new URL("/", req.body.url);
-  dns.lookup(urlbody.hostname, (err, address) => {
-    if (err || !address) {
+  dns.lookup(req.body.url.replace(/https?:\/\//, ""), (err) => {
+    if (err) {
       return res.json({ error: "invalid url" });
     }
-    const newUrl = new Url({ url: urlbody });
-    newUrl
-      .save()
-      .then((doc) => {
-        console.log("successfully shortened");
-        return res.json({ original_url: urlbody, short_url: doc._id });
-      })
-      .catch((err) => console.log(err));
+    URL.findOne({ original_url: req.body.url }, (error, data) => {
+      if (error) return console.log(error);
+      if (!data) {
+        const short_url = Math.floor(Math.random() * 90000) + 10000;
+        const original_url = req.body.url;
+        const newUrl = new URL({
+          short_url,
+          original_url,
+        });
+        newUrl.save((error, data) => {
+          if (error) return console.log(error);
+          return res.json({
+            original_url,
+            short_url,
+          });
+        });
+      } else {
+        let { short_url, original_url } = data;
+        return res.json({
+          original_url,
+          short_url,
+        });
+      }
+    });
   });
 }
 
 function navigateToUrl(req, res) {
-  const urlId = req.params.id;
-  Url.findById(urlId)
-    .then((data) => {
-      if (!data) {
-        return res.json({ error: "invalid url" });
-      }
-      return res.redirect(data.url);
-    })
-    .catch((err) => console.log(err));
+  URL.findOne({ short_url: req.params.url }, (error, data) => {
+    if (error) return console.log(error);
+    console.log(data);
+    if (!data) {
+      return res.json({ error: "No short URL found for the given input" });
+    } else {
+      let redirectUrl = data.original_url;
+
+      return res.redirect(redirectUrl);
+    }
+  });
 }
 
 module.exports = {
